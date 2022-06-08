1b5b5fb825d539eff9d3a1b70852593f8b7e4d27
Thor Bøje Simonsen <thor_simonsen@hotmail.com>
2022-06-08 14:46:09 +0200

    loginpage done

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, session, abort, request, flash
+import os
 import pandas as pd
 
 app = Flask(__name__ , static_url_path='/static')
@@ -6,10 +7,25 @@ app = Flask(__name__ , static_url_path='/static')
 data = pd.read_csv("attributes.csv", dtype=str)
 
 
-print(data.values)
 @app.route("/")
 def home():
-    return render_template("index.html", content=data.values)
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template("index.html", content=data.values)
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
 
 @app.route("/punk/<punkid>")
 def punkpage(punkid):
@@ -24,4 +40,5 @@ def punkpage(punkid):
     return render_template("cryptopunk.html", content=ct)
 
 if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
     app.run()
