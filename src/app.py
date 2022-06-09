036cfa87ee1e42cf3c7ef199d78e3e50099aa458
Thor Bøje Simonsen <thor_simonsen@hotmail.com>
2022-06-09 11:35:10 +0200

    query box almost done

--- src/app.py
+++ src/app.py
@@ -8,15 +8,48 @@ app = Flask(__name__ , static_url_path='/static')
 data = pd.read_csv("attributes.csv", dtype=str)
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        punks = data.values[:100]
+        if request.method == "POST":
+            input_gender = request.form["radio"]
+            input_type = request.form["radiotype"]
+            input_skin = request.form["radioskin"]
+
+            input_count = request.form["accessCount"] or 0
+            input_access = request.form["access"] or "NaN"
+
+            return redirect(url_for("querypage", gender=input_gender, types=input_type, skin=input_skin, access=input_access, count=input_count))
+            
+        punks = data.values[:10]
         length = len(punks)
         randomNumber = random.choice([id[0] for id in data.values])
         return render_template("index.html", content=punks, length=length, randomNumber = randomNumber)
+
+@app.route("/punks/<gender>/<types>/<skin>/<count>/<access>")
+def querypage(gender, types, skin, count, access):
+    ct = data.copy()
+    if gender != "Both":
+        ct = ct[ct["gender"] == gender]
+
+    if types != "All":
+        ct = ct[ct["type"] == types]
+
+    if skin != "All":
+        ct = ct[ct["skin tone"] == skin]
+
+    if access != "NaN":
+        ct = ct[ct["accessories"].str.contains(access)]
+    print(ct)
+    if count != 0:
+        ct = ct[ct["count"] == count]
+
+    ct = list(ct.values)[1:]
+    length = len(ct)
+    return render_template("cryptoquery.html", content=ct, length=length)
+
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
@@ -24,7 +57,7 @@ def do_admin_login():
         session['logged_in'] = True
     else:
         flash('wrong password!')
-    return home()
+    return redirect(url_for("home"))
 
 @app.route("/logout")
 def logout():
